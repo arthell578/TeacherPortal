03378ab43262a4c624a4f5c626aa5874d729fe8a
Marcin <Marcin@DESKTOP-L62L7NR>
2023-04-15 15:10:51 +0200

    Add register method

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,6 +1,25 @@
-﻿namespace TeacherPortal.Services
+﻿using TeacherPortal.Models;
+
+namespace TeacherPortal.Services
 {
     public class UserService
     {
+        private readonly TeacherPortalDbContext _dbContext;
+
+        public UserService(TeacherPortalDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public void Register(RegisterUserDTO registerUserDTO)
+        {
+            var newUser = new User()
+            {
+                Email= registerUserDTO.Email,
+                UserRole= registerUserDTO.UserRole,
+            };
+
+            _dbContext.Users.Add(newUser);
+            _dbContext.SaveChanges();
+        }
     }
 }
