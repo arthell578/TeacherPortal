044feda76e2c10100f1eff18b5df72b94deb2794
Marcin <Marcin@DESKTOP-L62L7NR>
2023-04-15 15:03:56 +0200

    Add data annotations

--- Models/RegisterUserDTO.cs
+++ Models/RegisterUserDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace TeacherPortal.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeacherPortal.Models
 {
     public class RegisterUserDTO
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
         public UserRole UserRole { get; set; } = UserRole.Student;
     }
