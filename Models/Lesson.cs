c064e15b7c58dca9aabd2a4824df5cf2e7ea1b6b
Marcin <Marcin@DESKTOP-L62L7NR>
2023-04-15 14:10:18 +0200

    Add lesson entity

---
+++ Models/Lesson.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeacherPortal.Models
+{
+    public class Lesson
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [MaxLength(255)]
+        public string AttachmentUrl { get; set; }
+
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
