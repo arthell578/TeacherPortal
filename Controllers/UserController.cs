be5fcdec137556aeb375aa1dd12b9b4347c06ed2
Marcin <Marcin@DESKTOP-L62L7NR>
2023-04-15 15:01:13 +0200

    Add user controller and register method

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeacherPortal.Controllers
+{
+    [ApiController]
+    [Route("/api/account")]
+    public class UserController
+    {
+        [HttpPost("/register")]
+        public ActionResult Register([FromBody] UserRegisterDTO)
+        {
+
+        }
+    }
+}
