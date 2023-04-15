c826f8592ea1e9aea3981f4c4335828cf98a228b
Marcin <Marcin@DESKTOP-L62L7NR>
2023-04-15 15:15:28 +0200

    Inject user service

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using TeacherPortal.Interfaces;
 using TeacherPortal.Models;
 
 namespace TeacherPortal.Controllers
 {
     [ApiController]
     [Route("/api/account")]
-    public class UserController
+    public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         [HttpPost("/register")]
         public ActionResult Register([FromBody] RegisterUserDTO registerUserDTO)
         {
-            return Ok(registerUserDTO);
+
         }
     }
 }
