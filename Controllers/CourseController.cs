714ee505886b795b12489d7f2179263860c9ade5
Marcin <Marcin@DESKTOP-L62L7NR>
2023-05-03 10:00:07 +0200

    Implement controller methods

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TeacherPortal.Interfaces;
+using TeacherPortal.Models;
 
 namespace TeacherPortal.Controllers
 {
@@ -12,6 +13,53 @@ namespace TeacherPortal.Controllers
         public CourseController(ICourseService courseService)
         {
             _courseService = courseService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CourseDTO>> CreateCourse([FromBody] CreateCourseDTO courseDTO)
+        {
+            var course = await _courseService.CreateCourseAsync(courseDTO);
+            return CreatedAtAction(nameof(GetCourse), new { id = course.Id }, course);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CourseDTO>>> GetAllCourses()
+        {
+            var courses = await _courseService.GetAllCoursesAsync();
+            return Ok(courses);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CourseDTO>> GetCourse(int id)
+        {
+            var course = await _courseService.GetCourseByIdAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(course);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<CourseDTO>> UpdateCourse(int id, [FromBody] CreateCourseDTO courseDTO)
+        {
+            var course = await _courseService.UpdateCourseAsync(id, courseDTO);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(course);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse(int id)
+        {
+            await _courseService.DeleteCourseAsync(id);
+            return NoContent();
         }
     }
 }
