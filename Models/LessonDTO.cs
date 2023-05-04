e063b859942ef8804993ab00975dd1c509d104cf
Marcin <Marcin@DESKTOP-L62L7NR>
2023-05-04 20:20:11 +0200

    Add lessonDTO properties

--- Models/LessonDTO.cs
+++ Models/LessonDTO.cs
@@ -2,5 +2,9 @@
 {
     public class LessonDTO
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int CourseId { get; set; }
     }
 }
