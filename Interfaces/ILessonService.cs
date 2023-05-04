f2be9a19697e036b1df228a9711eb5c755199cfe
Marcin <Marcin@DESKTOP-L62L7NR>
2023-05-04 20:21:33 +0200

    Add interface method definitions

--- Interfaces/ILessonService.cs
+++ Interfaces/ILessonService.cs
@@ -1,6 +1,13 @@
-﻿namespace TeacherPortal.Interfaces
+﻿using TeacherPortal.Models;
+
+namespace TeacherPortal.Interfaces
 {
-    public class ILessonService
+    public interface ILessonService
     {
+        Task<LessonDTO> CreateLessonAsync(CreateLessonDTO lessonDTO);
+        Task<IEnumerable<LessonDTO>> GetAllLessonsAsync();
+        Task<LessonDTO> GetLessonByIdAsync(int id);
+        Task<LessonDTO> UpdateLessonAsync(int id, UpdateLessonDTO lessonDTO);
+        Task DeleteLessonAsync(int id);
     }
 }
