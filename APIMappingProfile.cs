fc8de70f72ab302c617de093db75e9432df1fdc9
Marcin <Marcin@DESKTOP-L62L7NR>
2023-05-04 20:26:26 +0200

    Add lessons mapping profiles

--- APIMappingProfile.cs
+++ APIMappingProfile.cs
@@ -10,6 +10,9 @@ namespace TeacherPortal
             CreateMap<User, UserDTO>();
             CreateMap<CreateCourseDTO, Course>();
             CreateMap<Course, CourseDTO>();
+            CreateMap<CreateLessonDTO, Lesson>();
+            CreateMap<Lesson, LessonDTO>();
+            CreateMap<UpdateLessonDTO, Lesson>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
         }
     }
 }
