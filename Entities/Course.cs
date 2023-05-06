9fe25df5cd5e67451b7acbaf5dbc2d6dfe94fbf9
Marcin <Marcin@DESKTOP-L62L7NR>
2023-05-06 21:28:05 +0200

    Remove not used imports

--- Entities/Course.cs
+++ Entities/Course.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using TeacherPortal.Models;
 
 namespace TeacherPortal.Entities
 {
--- Entities/Lesson.cs
+++ Entities/Lesson.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using TeacherPortal.Models;
 
 namespace TeacherPortal.Entities
 {
